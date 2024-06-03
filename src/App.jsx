21bc6bafcdb077b84e7243264e58ea104ea7428c
Manish Gupta <manishgupta9205@gmail.com>
2024-06-03 22:56:28 +0530

    creating basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Navbar from "./assets/components/Navbar";
+import Footer from "./assets/components/Footer";
+import Home from "./assets/pages/Home";
 
-import './App.css'
-
-function App() {
-
+const Main = () => {
   return (
     <>
- <h1>Welcome to WordLand </h1>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
+};
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <>
+      <RouterProvider router={route}>
+        <h1>Welcome to WordLand </h1>
+      </RouterProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
